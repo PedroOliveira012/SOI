9224c893d9de6fdd1ae8d253401539d8def7eb6d
PedroOliveira012 <pedro120403@gmail.com>
2025-10-16 13:40:55 -0300

    Add vertical tab UI for kit editing form
    Introduces a vertical tab interface in the kit editing form to allow selection and editing of kits by amperage range. Updates CSS to style the new tab layout and modifies the Blade template to render tabs dynamically based on the kit's rangeAmperes. Also updates index page scripts, though some script tags appear to reference CSS files and duplicate jQuery includes.

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -16,9 +16,11 @@
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://kit.fontawesome.com/05b56d1101.js" crossorigin="anonymous"></script>
-        <script src="{{ asset('js/modernizr.custom.js') }}"></script>
-        <script src="{{ asset('js/cbpFWTabs.js') }}"></script>
+        
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
--- resources/views/kits/kits-editing-form.blade.php
+++ resources/views/kits/kits-editing-form.blade.php
@@ -2,6 +2,34 @@
 
 @section('content')
 <div class="container">
-    <h1 class="m-5 text-center">{{ $kit->area }}</h1>
+    <h1 class="m-5">{{ $kit->area }}</h1>
+
+        <div class="d-flex justify-content-center mt-20">
+            <div class="mb-5 w-100">      
+                <div class="tab-vertical d-flex justify-content-center">
+                    <div class="tabs-div">
+                        <ul class="nav nav-tabs overflow-y-scroll" id="myTab3" role="tablist">
+                        @foreach ($kit->rangeAmperes as $tab)
+                            <li class="nav-item">
+                                <a class="nav-link" id="home-vertical-tab" data-toggle="tab" href="#{{$tab}}-tab" role="tab" aria-controls="home" aria-selected="true">
+                                    {{ $tab }}A
+                                </a>
+                            </li>
+                        @endforeach
+                        </ul>
+                    </div>
+                    <div class="tab-content" id="myTabContent3">
+                        <div class="tab-pane show active tab-pane-placeholder" role="tabpanel" aria-labelledby="home-vertical-tab">
+                            <p>Selecione um kit para a edição</p>
+                        </div>
+                    @foreach ($kit->rangeAmperes as $tab)
+                        <div class="tab-pane show" id="{{$tab}}-tab" role="tabpanel" aria-labelledby="home-vertical-tab">
+                            <p>{{$tab}}</p>
+                        </div>
+                    @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
 </div>
 @endsection
